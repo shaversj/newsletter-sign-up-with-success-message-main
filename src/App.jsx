aa918c3b0a99de1d8507bb09b5c974532f87695a
Josh Shavers <shaversj@gmail.com>
2023-11-08 07:33:25 -0600

    Added changes to support desktop layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,19 @@ function App() {
     <>
         <div className={"bg-black min-h-screen flex items-center"}>
             <div className={"max-w-screen-sm w-full lg:max-w-screen-lg bg-charcoal-grey mx-auto lg:min-h-[1080px] lg:max-h-[1080px] lg:flex lg:justify-center lg:items-center"}>
-                <main className={" lg:w-[928px] lg:min-h-[641px] bg-white lg:rounded-[2.25rem] font-Roboto text-charcoal-grey"}>
-                    <div>
-                        <img src={mobileImage} alt={""} />
+                <main className={" lg:w-[928px] lg:min-h-[641px] lg:grid lg:grid-rows-1 lg:grid-cols-[54.2%,45.8%] lg:items-center  bg-white lg:rounded-[2.25rem] font-Roboto text-charcoal-grey"}>
+                    <div className={"lg:col-start-2 lg:col-end-2 "}>
+                        {/*<img src={mobileImage} alt={""} />*/}
+                        <picture>
+                            <source className={""} srcSet={desktopImage} media="(min-width: 1440px)" />
+                            <img className={""} src={mobileImage} alt="" />
+                        </picture>
                     </div>
 
-                    <div className={"px-6 pt-8 pb-9"}>
-                        <h1 className={"text-[40px] font-semibold"}>Stay updated!</h1>
-                        <p className={"pt-3"}>Join 60,000+ product managers receiving monthly updates on:</p>
-                        <ul className={"space-y-3 pt-6 "}>
+                    <div className={"px-6 pt-8 pb-9 lg:row-start-1 lg:col-start-1 lg:col-end-1 lg:px-[63px] lg:pt-0 lg:pb-4"}>
+                        <h1 className={"text-[40px] font-semibold lg:text-[56px]"}>Stay updated!</h1>
+                        <p className={"pt-3 lg:pt-2"}>Join 60,000+ product managers receiving monthly updates on:</p>
+                        <ul className={"space-y-3 pt-6 lg:space-y-[.7rem]"}>
                             <li className={"bg-[url('./src/assets/images/icon-list.svg')] bg-no-repeat pl-9"}>Product discovery and building what matters</li>
                             <li className={"bg-[url('./src/assets/images/icon-list.svg')] bg-no-repeat pl-9"}>Measuring to ensure updates are a success</li>
                             <li className={"bg-[url('./src/assets/images/icon-list.svg')] bg-no-repeat pl-9"}>And much more!</li>
