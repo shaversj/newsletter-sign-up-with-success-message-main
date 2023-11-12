5005d5eab8f7ed19efe9237521a42fa38bbfae92
Josh Shavers <shaversj@gmail.com>
2023-11-12 07:33:15 -0600

    Add gh-pages support

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,15 +1,18 @@
-import iconSuccess from './assets/images/icon-success.svg'
-const Modal = ({emailAddress, toggleModal}) => {
-    return (
-        <main className={"px-6 pt-[147px] pb-10 lg:px-16 lg:py-12 lg:w-[504px] lg:h-[520px] lg:items-center  bg-white lg:rounded-[2.25rem] font-Roboto text-charcoal-grey"}>
-            <img className={""} src={iconSuccess} alt={""} />
-            <h1 className={"pt-10 text-[40px] leading-[2.5rem] font-semibold lg:pt-10 lg:text-[56px] lg:leading-[3.5rem] lg:font-bold"}>Thanks for subscribing!</h1>
-            <p className={"pt-6 lg:pt-6"}>A confirmation email has been sent to <span className={"font-bold"}>{emailAddress}</span>. Please open it and click the button inside to confirm your subscription</p>
-            <button className={"mt-[265px] lg:mt-10 w-full h-[56px] bg-dark-slate-grey rounded-lg hover:bg-[url('./src/assets/images/illustration-sign-up-desktop.svg')] hover:bg-bottom shadow-2xl"} onClick={toggleModal}>
-                <span className={"text-white font-medium"}>Dismiss message</span>
-            </button>
-        </main>
-    );
+import iconSuccess from "./assets/images/icon-success.svg";
+
+const Modal = ({ emailAddress, toggleModal }) => {
+  return (
+    <main className={"px-6 pt-[147px] pb-10 lg:px-16 lg:py-12 lg:w-[504px] lg:h-[520px] lg:items-center  bg-white lg:rounded-[2.25rem] font-Roboto text-charcoal-grey"}>
+      <img className={""} src={iconSuccess} alt={""} />
+      <h1 className={"pt-10 text-[40px] leading-[2.5rem] font-semibold lg:pt-10 lg:text-[56px] lg:leading-[3.5rem] lg:font-bold"}>Thanks for subscribing!</h1>
+      <p className={"pt-6 lg:pt-6"}>
+        A confirmation email has been sent to <span className={"font-bold"}>{emailAddress}</span>. Please open it and click the button inside to confirm your subscription
+      </p>
+      <button className={"mt-[265px] lg:mt-10 w-full h-[56px] bg-dark-slate-grey rounded-lg hover:bg-[url('./src/assets/images/illustration-sign-up-desktop.svg')] hover:bg-bottom shadow-2xl"} onClick={toggleModal}>
+        <span className={"text-white font-medium"}>Dismiss message</span>
+      </button>
+    </main>
+  );
 };
 
 export default Modal;
